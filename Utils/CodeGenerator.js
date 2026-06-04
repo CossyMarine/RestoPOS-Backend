6b616a2c2c561f74eddf7ed252b1b232032e540f
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 21:48:54 +0300

    Update CodeGenerator.js

--- Utils/CodeGenerator.js
+++ Utils/CodeGenerator.js
@@ -1,16 +1,12 @@
-const crypto = require('crypto');
+import crypto from "crypto";
 
-/**
- * Generates a random alphanumeric reward code of specified length.
- * @param {number} length - Length of the reward code
- * @returns {string} - The generated reward code
- */
 const generateRewardCode = (length = 8) => {
-  return crypto.randomBytes(length)
-    .toString('base64')      // Converts to Base64 string
-    .replace(/[^a-zA-Z0-9]/g, '') // Remove special characters
-    .slice(0, length)         // Trim to desired length
-    .toUpperCase();           // Uppercase for readability
+  return crypto
+    .randomBytes(length)
+    .toString("base64")
+    .replace(/[^a-zA-Z0-9]/g, "")
+    .slice(0, length)
+    .toUpperCase();
 };
 
-module.exports = generateRewardCode;
+export default generateRewardCode;
