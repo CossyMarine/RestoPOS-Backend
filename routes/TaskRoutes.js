f6a987fa61d4c79ffa1839b90af514b3767c9ffd
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 22:05:56 +0300

    Update TaskRoutes.js

--- routes/TaskRoutes.js
+++ routes/TaskRoutes.js
@@ -1,19 +1,12 @@
-// routes/TaskRoutes.js
-const express = require("express");
-const router = express.Router();
-const {
-  createTask,
-  getTasks,
-  getTaskById,
-} = require("../Controllers/taskcontroller");
-const { protect } = require("../Middlewares/authMiddleware");
-const roleMiddleware = require("../Middlewares/roleMiddleware");
+import { Router } from "express";
+import { createTask, getTasks, getTaskById } from "../Controllers/taskcontroller.js";
+import { protect } from "../Middlewares/authMiddleware.js";
+import roleMiddleware from "../Middlewares/roleMiddleware.js";
 
-// Public Routes
+const router = Router();
+
 router.get("/", getTasks);
 router.get("/:id", getTaskById);
-
-// Admin Route to create task
 router.post("/", protect, roleMiddleware("admin"), createTask);
 
-module.exports = router;
+export default router;
