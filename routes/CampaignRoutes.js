0d8b98b49451ef847c2021b1ff17caf752d2f343
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 22:03:48 +0300

    Update CampaignRoutes.js

--- routes/CampaignRoutes.js
+++ routes/CampaignRoutes.js
@@ -1,22 +1,17 @@
-const router = require("express").Router();
-const { protect } = require("../Middlewares/authMiddleware");
-const role = require("../Middlewares/roleMiddleware");
-const C = require("../Controllers/CampaignController");
+import { Router } from "express";
+import { protect } from "../Middlewares/authMiddleware.js";
+import role from "../Middlewares/roleMiddleware.js";
+import * as C from "../Controllers/CampaignController.js";
 
-// Public browse
+const router = Router();
+
 router.get("/", C.listActive);
-
-// Poster flow
-router.post("/", protect, C.createCampaign);                             // create draft
-router.put("/:id/fund-activate", protect, C.fundAndActivate);            // lock escrow & go live
+router.post("/", protect, C.createCampaign);
+router.put("/:id/fund-activate", protect, C.fundAndActivate);
 router.put("/:id/pause", protect, C.pauseCampaign);
 router.put("/:id/resume", protect, C.resumeCampaign);
 router.put("/:id/stop", protect, C.stopCampaign);
-
-// Earner submits
 router.post("/:id/submit", protect, C.submitProof);
-
-// Poster reviews
 router.put("/:id/submissions/:submissionId/review", protect, C.reviewSubmissionByPoster);
 
-module.exports = router;
+export default router;
