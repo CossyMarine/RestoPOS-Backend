33fac2eb06a7880ee5c2e8c7d1098ad9cab29f73
marinehaley3 <marinehaley3@gmail.com>
2026-06-04 22:04:40 +0300

    Update ModeratorRoutes.js

--- routes/ModeratorRoutes.js
+++ routes/ModeratorRoutes.js
@@ -1,22 +1,14 @@
-const express = require("express");
-const router = express.Router();
-const { protect } = require("../Middlewares/authMiddleware");
-const roleMiddleware = require("../Middlewares/roleMiddleware");
+import { Router } from "express";
+import { protect } from "../Middlewares/authMiddleware.js";
+import roleMiddleware from "../Middlewares/roleMiddleware.js";
+import { reviewTask, approveTask, rejectTask, viewReports, viewUsers } from "../Controllers/ModeratorController.js";
 
-const {
-  reviewTask,
-  approveTask,
-  rejectTask,
-  viewReports,
-  viewUsers,
-} = require("../Controllers/ModeratorController");
+const router = Router();
 
-// ✅ Moderator-only routes
 router.get("/review-tasks", protect, roleMiddleware("moderator"), reviewTask);
 router.put("/approve-task/:taskId", protect, roleMiddleware("moderator"), approveTask);
 router.put("/reject-task/:taskId", protect, roleMiddleware("moderator"), rejectTask);
-router.get("/view-users", protect, roleMiddleware ("moderator"), viewUsers);
-// Reports from users
+router.get("/view-users", protect, roleMiddleware("moderator"), viewUsers);
 router.get("/reports", protect, roleMiddleware("moderator"), viewReports);
 
-module.exports = router;
+export default router;
