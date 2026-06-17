21677a21b09ee1538b2d55a279e73fac2b2cc706
marinesaili461 <marinesaili461@gmail.com>
2026-06-17 18:22:32 +0300

    Update AuthRoutes.js

--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -5,11 +5,7 @@ import { protect } from "../Middlewares/authMiddleware.js";
 const router = Router();
 
 router.post("/register", AuthController.register);
-router.get("/verify", AuthController.verifyEmail);
 router.post("/login", AuthController.login);
-router.post("/forgot-password", AuthController.forgotPassword);
-router.post("/reset-password/:token", AuthController.resetPassword);
 router.get("/me", protect, AuthController.getProfile);
-router.post("/resend-verification", AuthController.resendVerification);
 
 export default router;
