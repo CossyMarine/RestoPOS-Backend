fe202e8f47058bf175bc034a99430d7358413667
CossyMarine <shidayawatukibwari@gmail.com>
2026-07-11 02:13:36 +0300

    Delete Utils directory

--- Utils/Logger.js
+++
@@ -1,9 +0,0 @@
-import AuditLog from "../models/AuditLog.js";
-
-export const createLog = async (adminId, action, details, ipAddress) => {
-  try {
-    await AuditLog.create({ admin: adminId, action, details, ipAddress });
-  } catch (error) {
-    console.error("Audit Log Error:", error.message);
-  }
-};
