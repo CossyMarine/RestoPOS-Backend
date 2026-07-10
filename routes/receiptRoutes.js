21cfd383d30de59ec0c7d8fcf46e034586fd17b2
CossyMarine <shidayawatukibwari@gmail.com>
2026-07-11 02:19:49 +0300

    Create receiptRoutes.js

---
+++ routes/receiptRoutes.js
@@ -0,0 +1,12 @@
+// routes/receiptRoutes.js
+import express from "express";
+import { payReceipt, getReceipts, getReceiptsByWaiter } from "../controllers/receiptController.js";
+import { protect, authorize } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.patch("/:id/pay", protect, authorize("admin", "manager", "cashier"), payReceipt);
+router.get("/", protect, authorize("cashier", "manager", "admin"), getReceipts);
+router.get("/waiter/:name", protect, getReceiptsByWaiter);
+
+export default router;
